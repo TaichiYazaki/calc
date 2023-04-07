7db2a6f078fe92c17b2cc803ebfaaf8b8fa54cc3
TaichiYazaki <taichi.yazaki@rakus-partners.co.jp>
2023-04-07 12:59:34 +0900

    基本機能の完成

--- main.go
+++ main.go
@@ -4,14 +4,14 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
 )
 
-type Page struct {
-	Title string
-	Body  []byte
+func handler(w http.ResponseWriter, r *http.Request, result int) {
+
 }
 
-func viewhandler(w http.ResponseWriter, r *http.Request) {
+func viewHandler(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("template/view.html")
 
 	if err != nil {
@@ -23,7 +23,53 @@ func viewhandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func calcHandler(w http.ResponseWriter, r *http.Request) {
+	var num1 int
+	var num2 int
+	num1, _ = strconv.Atoi(r.FormValue("num1"))
+	num2, _ = strconv.Atoi(r.FormValue("num2"))
+	op := r.FormValue("op")
+	if op == "add" {
+		result := num1 + num2
+		t, err := template.ParseFiles("template/view.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := t.Execute(w, result); err != nil {
+			log.Fatal(err)
+		}
+	} else if op == "sub" {
+		result := num1 - num2
+		t, err := template.ParseFiles("template/view.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := t.Execute(w, result); err != nil {
+			log.Fatal(err)
+		}
+	} else if op == "mult" {
+		result := num1 * num2
+		t, err := template.ParseFiles("template/view.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := t.Execute(w, result); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		result := num1 / num2
+		t, err := template.ParseFiles("template/view.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := t.Execute(w, result); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
 func main() {
-	http.HandleFunc("/", viewhandler)
+	http.HandleFunc("/", viewHandler)
+	http.HandleFunc("/calc", calcHandler)
 	http.ListenAndServe(":8080", nil)
 }
